759f1be1626464a2f02832fc72bf26faf6cd46bb
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-02-21 12:21:07 +0530

    added Routes and Controllers for create tweet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import connect from './config/database.js';
+import bodyParser from 'body-parser';
 
+import connect from './config/database.js';
+import apiRoutes from './routes/index.js';
 
 const app = express();
-app.use(express.urlencoded());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/api', apiRoutes);
 
 
 app.listen(3000, async () => {
