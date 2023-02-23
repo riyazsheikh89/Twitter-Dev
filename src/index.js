18f73937c6741a127e5a564c931068e917d1c153
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-02-23 15:56:56 +0530

    added toggleLike api

--- src/index.js
+++ src/index.js
@@ -20,15 +20,4 @@ app.listen(3000, async () => {
     await connect();
     console.log("MongoDB Connected");
 
-    const userRepo = new UserRepository();
-    const tweetRepo = new TweetRepository();
-
-    const tweets = await tweetRepo.getAll(0, 10);
-    const users = await userRepo.getAll();
-
-    const likeService = new LikeService();
-    const ans = await likeService.toggleLike(tweets[0].id, 'Tweet', users[0].id);
-    console.log(`Like Added on Tweet: ${ans}`);
-    
-
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {createTweet} from '../../controllers/tweet-controller.js'
+import {toggleLike} from '../../controllers/like-controller.js';
 
 const router = express.Router();
 
 router.post('/tweets', createTweet);    // create new tweet
+router.post('/likes/toggle', toggleLike);   //like or dislike tweet
 
 export default router;
 
