1fc023c9569ab36534d005b0a7842993a13a0781
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2023-02-23 16:28:34 +0530

    updated Comment & Tweet models

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,13 +1,31 @@
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 
 const commentSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true
     },
-    userEmail: {
-        type: String
-    }
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'onModel',
+        required: true
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
